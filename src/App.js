f7d34e52b15d8877c557990abebafdd3b495558e
unknown <singhhardy724@gmail.comcleargit config --global user.name Hardik>
2023-05-08 00:10:56 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
+import About from './About';
+import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
     <h1> hello test app</h1>
+    <Router>
+        <div className="app">
+          <div className='content'>
+            <Routes>
+              <Route path='/' exact element={<Home />} />
+              <Route path='/About' element={<About />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
